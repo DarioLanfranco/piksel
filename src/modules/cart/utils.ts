868998ef0fff2f0eee665b479ce3a5a68d655ea2
Dario Lanfranco <dariolanfrancoruffener@gmail.com>
2026-06-23 09:42:18 -0300

    piksel-check-8

---
+++ src/modules/cart/utils.ts
@@ -0,0 +1,8 @@
+export function formatPrice(n: number): string {
+  return new Intl.NumberFormat('es-AR', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 0,
+  }).format(n);
+}
